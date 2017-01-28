42a59841f5334d9807701212120f1e8809309b94
redaxmedia <info@redaxmedia.com>
2017-01-28 13:12:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Redaxmedia template
+===================
+
+> Template for Redaxmedia's website.
+
+[![Build Status](https://img.shields.io/travis/redaxmedia/redaxmedia-template.svg)](https://travis-ci.org/redaxmedia/redaxmedia-template)
+[![Dependency Status](https://gemnasium.com/badges/github.com/redaxmedia/redaxmedia-template.svg)](https://gemnasium.com/github.com/redaxmedia/redaxmedia-template)
---
+++ gruntfile.js
@@ -0,0 +1,208 @@
+module.exports = function (grunt)
+{
+	'use strict';
+
+	/* config grunt */
+
+	grunt.initConfig(
+	{
+		version: grunt.file.readJSON('package.json').version,
+		jsonlint:
+		{
+			dependency:
+			{
+				src:
+				[
+					'composer.json',
+					'package.json'
+				]
+			},
+			ruleset:
+			{
+				src:
+				[
+					'.htmlhintrc',
+					'.stylelintrc',
+					'.tocgen'
+				]
+			}
+		},
+		htmlhint:
+		{
+			templates:
+			{
+				src:
+				[
+					'templates/**/*.phtml'
+				]
+			},
+			options:
+			{
+				htmlhintrc: '.htmlhintrc'
+			}
+		},
+		postcss:
+		{
+			templateRedaxmedia:
+			{
+				src:
+				[
+					'templates/redaxmedia/assets/styles/_redaxmedia.css'
+				],
+				dest: 'templates/redaxmedia/dist/styles/redaxmedia.min.css'
+			},
+			stylelint:
+			{
+				src:
+				[
+					'templates/*/assets/styles/*.css'
+				],
+				options:
+				{
+					processors:
+					[
+						require('stylelint'),
+						require('postcss-reporter')(
+						{
+							throwError: true
+						})
+					]
+				}
+			},
+			stylefmt:
+			{
+				src:
+				[
+					'templates/*/assets/styles/*.css'
+				],
+				options:
+				{
+					processors:
+					[
+						require('stylefmt')
+					]
+				}
+			},
+			colorguard:
+			{
+				src:
+				[
+					'templates/*/dist/styles/*.css'
+				],
+				options:
+				{
+					processors:
+					[
+						require('colorguard')(
+						{
+							threshold: 2,
+							allowEquivalentNotation: true
+						}),
+						require('postcss-reporter')(
+						{
+							throwError: true
+						})
+					]
+				}
+			},
+			options:
+			{
+				processors:
+				[
+					require('postcss-import'),
+					require('postcss-custom-properties'),
+					require('postcss-custom-media'),
+					require('postcss-custom-selectors'),
+					require('postcss-nesting'),
+					require('postcss-extend'),
+					require('postcss-color-gray'),
+					require('postcss-color-function'),
+					require('autoprefixer')(
+					{
+						browsers: 'last 2 versions'
+					}),
+					require('cssnano')(
+					{
+						autoprefixer: false,
+						colormin: false,
+						zindex: false
+					})
+				]
+			}
+		},
+		shell:
+		{
+			tocTemplates:
+			{
+				command: 'sh vendor/bin/tocgen.sh templates/redaxmedia/assets/styles .tocgen'
+			},
+			toclintTemplates:
+			{
+				command: 'vendor/bin/tocgen.sh templates/redaxmedia/assets/styles .tocgen -l'
+			},
+			options:
+			{
+				stdout: true,
+				failOnError: true
+			}
+		},
+		watch:
+		{
+			styles:
+			{
+				files:
+				[
+					'assets/styles/*.css',
+					'templates/**/assets/styles/*.css'
+				],
+				tasks:
+				[
+					'build-styles'
+				]
+			}
+		}
+	});
+
+	/* load tasks */
+
+	require('load-grunt-tasks')(grunt);
+
+	/* register tasks */
+
+	grunt.registerTask('default',
+	[
+		'jsonlint',
+		'stylelint',
+		'colorguard',
+		'htmlhint',
+		'toclint'
+	]);
+	grunt.registerTask('stylelint',
+	[
+		'postcss:stylelint'
+	]);
+	grunt.registerTask('stylefmt',
+	[
+		'postcss:stylefmt'
+	]);
+	grunt.registerTask('colorguard',
+	[
+		'postcss:colorguard'
+	]);
+	grunt.registerTask('toclint',
+	[
+		'shell:toclintTemplates'
+	]);
+	grunt.registerTask('toc',
+	[
+		'shell:tocTemplates'
+	]);
+	grunt.registerTask('build',
+	[
+		'build-styles'
+	]);
+	grunt.registerTask('build-styles',
+	[
+		'postcss:templateRedaxmedia'
+	]);
+};
